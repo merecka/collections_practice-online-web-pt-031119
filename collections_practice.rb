511f247ae546418f67a4429e8f4f514a89023dc4
Alex Merecka <merecka@gmail.com>
2019-03-21 23:18:13 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
+def sort_array_asc(numbers)
+  numbers.sort
+end
 
+def sort_array_desc(numbers)
+  numbers.sort{|x,y| y <=> x}
+end
+
+def sort_array_char_count(strings)
+  strings.sort{|x,y| strings[x].length <=> strings[y].length}
+end
